ab841dbf67327277d792864274dc991bb057461c
Ana Casa Grandi <130663158+anacasagrandi@users.noreply.github.com>
2024-08-01 16:08:14 -0300

    02

---
+++ 02.py
@@ -0,0 +1,11 @@
+#2. Elabore um programa que calcule o perímetro de um terreno 10 x 30 metros.
+# Armazenando valores/ Inputs
+print("Informe os valores para multiplicação\n")
+n1 = 10
+n2 = 30
+
+# Cálculo
+mult = n1 * n2
+
+# Exibir resultado
+print("O perímetro desse terreno é: {}.\n".format(mult))
