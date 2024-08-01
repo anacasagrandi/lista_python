e98c536656f1f9cf960abf38c832ca71021fafb6
Ana Casa Grandi <130663158+anacasagrandi@users.noreply.github.com>
2024-08-01 16:05:21 -0300

    1 py

---
+++ 01.py
@@ -0,0 +1,18 @@
+# Faça um algoritmo que mostre: nome, idade, altura, peso e cidade.
+#variaveis
+nome = "Silvete"
+idade = "17"
+altura = "1,70"
+peso = "82"
+cidade = "São Longuinho"
+ #exibição de valores
+print(nome)
+print(idade)
+print(altura)
+print(peso)
+print(cidade)
+
+
+#3 f string
+print(f"O meu nome é: {nome}, tenho {idade} anos, meço {altura} de altura pesando {peso} quilos e moro em {cidade}")
+print("\n")
